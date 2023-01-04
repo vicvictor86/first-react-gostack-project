08916924e3fb1be787db151d51e9011bfa80d354
Victor Emmanuel <vicbem@hotmail.com>
2023-01-03 23:42:15 -0300

    Styling the dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import Title from './styles';
+import logoImg from '../../assets/Logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img src="https://avatars.githubusercontent.com/u/45568289?v=4" alt="Victor" />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Descrição do repositório massa</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img src="https://avatars.githubusercontent.com/u/45568289?v=4" alt="Victor" />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Descrição do repositório massa</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
